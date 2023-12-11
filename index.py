6dcf66f845197594dce4abb52e0141bb527dc345
aleyna-soyer <aleynasoyer9@gmail.com>
2023-12-12 02:11:02 +0300

    feat: /posts created

--- index.py
+++ index.py
@@ -1,6 +1,27 @@
 from flask import Flask,render_template
+from mongoengine import connect, disconnect
+from dotenv import load_dotenv
+from post import Post
+import os
+import json 
 
 app=Flask(__name__)
+
+@app.route("/posts")
+def list_posts():
+    load_dotenv()
+    mongo_uri = os.getenv("MONGO_URI")
+    connect(host=mongo_uri)
+
+    all_posts = []
+    for p in Post.objects():
+        
+        all_posts.append(p.to_json())
+        disconnect()
+
+    return json.dumps(all_posts)    
+        
+
 
 @app.route("/")
 def homePage():
