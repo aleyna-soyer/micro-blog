8e29bd1c9076f69fb6914fea01ed1975e041c433
aleyna-soyer <aleynasoyer9@gmail.com>
2023-12-22 20:00:02 +0300

    feat: add register and login endpoints

--- index.py
+++ index.py
@@ -1,26 +1,47 @@
-from flask import Flask,render_template
+from functools import wraps
+from flask_bcrypt import Bcrypt
+from flask import Flask, jsonify, make_response,render_template, request
 from mongoengine import connect, disconnect
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from post import Post
+from user import User
 import os
 import json 
+import jwt
 
 app=Flask(__name__)
+load_dotenv()
+mongo_uri = os.getenv("MONGO_URI")
+connect(host=mongo_uri)
+bcrypt = Bcrypt(app)
+secret_key = os.getenv("SECRET_KEY")
 
-@app.route("/posts")
-def list_posts():
-    #load_dotenv()
-    mongo_uri = os.getenv("MONGO_URI")
-    connect(host=mongo_uri)
+@app.route('/register', methods=['POST'])
+def register(): 
+    username = request.json.get('username')
+    password = request.json.get('password')
 
-    all_posts = []
-    for p in Post.objects():
-        
-        all_posts.append(p.to_json())
-        disconnect()
+    password = bcrypt.generate_password_hash(password).decode('utf-8')
+    if username and password:                
+        user = User(username=username, password=password)
+        user.save()  
+    else:
+        return jsonify({'message': 'Fill in the required fields'} )
+    return jsonify({'message': 'registered successfully'})
 
-    return json.dumps(all_posts)     
+@app.route('/login', methods=['POST']) 
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
 
+    user = User.objects(username=username).first()
+
+    if not user or not bcrypt.check_password_hash(user.password, password):
+        return make_response("Wrong username or password", 401)
+
+    token = jwt.encode({'username': user.username}, secret_key)
+    return jsonify({'token': token})
+     
 @app.route("/")
 def homePage():
     return "My Blog Page"
