9f3301171af18f13f228013ea1a1f52c85cc7baf
aleyna-soyer <aleynasoyer9@gmail.com>
2023-12-27 00:05:09 +0300

    feat: add-addpost endpoint

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from functools import wraps
+import time
 from flask_bcrypt import Bcrypt
 from flask import Flask, jsonify, make_response,render_template, request
 from mongoengine import connect, disconnect
@@ -8,16 +10,59 @@ from user import User
 import os
 import json 
 import jwt
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 
 app=Flask(__name__)
 load_dotenv()
+app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY", "SECRET_KEY")
 mongo_uri = os.getenv("MONGO_URI")
 connect(host=mongo_uri)
 bcrypt = Bcrypt(app)
-secret_key = os.getenv("SECRET_KEY")
+jwt = JWTManager(app)
+
+@jwt.user_identity_loader
+def get_id_by_user(user):
+
+    """Callback function to get the identity of the current user."""
+    if isinstance(user, User):
+        return str(user.id)
+    
+    return None  # veya uygun bir değer döndürün
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+
+    """Callback function to look up a user by identity in JWT data."""
+    
+    identity = jwt_data["sub"]
+
+    return User.objects.get(id=identity)
+
+@app.route('/addpost', methods=['POST'])
+@jwt_required()
+def  addpost():
+
+    current_user_id = get_jwt_identity()
+    print(current_user_id)
+    if not current_user_id:
+        return make_response("login to post a new entry", 401)
+    user = User.objects.get(id=current_user_id)
+
+    if request.method == 'POST':
+        title = request.json.get('title')
+        content = request.json.get('content')
+
+        newPost = Post(
+            title=title, content=content, 
+            author=user, date=time.time() 
+            )
+        newPost.save()
+        
+        return jsonify({'message': 'create post successfully'})
 
 @app.route('/register', methods=['POST'])
 def register(): 
+
     username = request.json.get('username')
     password = request.json.get('password')
 
@@ -25,8 +70,10 @@ def register():
     if username and password:                
         user = User(username=username, password=password)
         user.save()  
+
     else:
         return jsonify({'message': 'Fill in the required fields'} )
+    
     return jsonify({'message': 'registered successfully'})
 
 @app.route('/login', methods=['POST']) 
@@ -35,13 +82,16 @@ def login():
     password = request.json.get('password')
 
     user = User.objects(username=username).first()
-
+    print(user)
+    print()
+    print(user.id)
+    print(app.config["JWT_SECRET_KEY"])
     if not user or not bcrypt.check_password_hash(user.password, password):
         return make_response("Wrong username or password", 401)
 
-    token = jwt.encode({'username': user.username}, secret_key)
+    token = create_access_token(identity=user)
     return jsonify({'token': token})
-     
+        
 @app.route("/")
 def homePage():
     return "My Blog Page"
